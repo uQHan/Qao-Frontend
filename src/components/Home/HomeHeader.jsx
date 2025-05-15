1833a74daaafa76e38d04f61770880900768deae
uQHan <hanng.22ite@vku.udn.vn>
2025-05-15 11:44:36 +0700

    feat: Enhance HomeHeader login functionality and update ProfileHeader styling for better UX

--- src/components/Home/HomeHeader.jsx
+++ src/components/Home/HomeHeader.jsx
@@ -2,13 +2,21 @@ import { BsArrowRepeat } from 'react-icons/bs'
 import { useBoundStore } from '@/store/useBoundStore'
 import { CgProfile } from "react-icons/cg";
 import playSound from '@/helpers/playSound'
+import { useRouter } from 'next/router';
 
 export default function HomeHeader() {
 	const { setDest } = useBoundStore(state => state)
+	const router = useRouter();
 	function handleLogin() {
-		playSound('pop')
-		setDest('profile')
-		document.getElementById('authDialog')?.showModal()
+		playSound('pop');
+		const user = JSON.parse(sessionStorage.getItem('user')); 
+		if (user) {
+			setDest(null); 
+			router.push('/profile'); 
+		} else {
+			setDest('profile'); 
+			document.getElementById('authDialog')?.showModal();
+		}
 	}
 	return (
 		<nav className='fixed right-4 top-3 z-20'>
@@ -16,11 +24,6 @@ export default function HomeHeader() {
 				<li>
 					<button href="/" className='block' onClick={handleLogin}>
 						<CgProfile color='#0f172a' className='text-4xl hover:scale-105 transition-all  p-1 bg-white rounded' title='Login' />
-					</button>
-				</li>
-				<li>
-					<button className='hover:scale-105 transition-all p-1 bg-white rounded' onClick={() => document.getElementById('newGameDialog').showModal()}>
-						<BsArrowRepeat className='text-[28px]' color='#0f172a' title='Redo' />
 					</button>
 				</li>
 			</ul>
--- src/components/Profile/ProfileHeader.jsx
+++ src/components/Profile/ProfileHeader.jsx
@@ -5,8 +5,8 @@ export default function ProfileHeader() {
 	const router = useRouter()
 
 	return (
-		<nav className='flex gap-4 p-4'>
-			<button onClick={() => router.push('/')} className='hover:scale-105 transition-all p-1 bg-white rounded'>
+		<nav className='flex gap-4 p-4 fixed top-2 left-2 z-50'>
+			<button onClick={() => router.push('/')} className='hover:scale-105 transition-all p-1 bg-white rounded shadow-lg'>
 				<BiArrowBack color='#0f172a' className='text-4xl' title='Go back' />
 			</button>
 		</nav>
